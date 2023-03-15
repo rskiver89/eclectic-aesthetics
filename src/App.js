308e14041235a89a2bfd3aeae9a4b8878fe58c7a
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-03-14 20:50:35 -0400

    add signup button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Homepage from './pages/Homepage/Homepage.js'
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.js'
 import CategoryArticles from './pages/CategoryArticle/CategoryArticles'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Auth from './pages/Auth/Auth'
-import AddArticle from './pages/AddArticle/AddArticle'
-import ArticleDetails from './pages/ArticleDetails/ArticleDetails'
+// import AddArticle from './pages/AddArticle/AddArticle'
+// import ArticleDetails from './pages/ArticleDetails/ArticleDetails'
 
 function App() {
+
+  const categories = ['Health', 'Food', 'Travel', 'Technology'];
+  
   return (
     <BrowserRouter>
-    <Header />
+    <Header categories={categories} />
     <Routes>
       <Route path='/' element={<Homepage />} />
-      <Route path='/category/:categoryName' element={<CategoryArticles/>}/>
-      <Route path='/auth' element={<Auth/>} />
+      <Route path='/category/:categoryName' element={<CategoryArticles />}/>
+      <Route path='/auth' element={<Auth />} />
     </Routes>
     </BrowserRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,51 +1,34 @@
 import React from 'react'
 import './Header.css'
-import { FaHome } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom'
-import {auth} from '../../config/firebaseConfig.js'
+import {FaHome} from 'react-icons/fa'
+import {Link, useNavigate} from 'react-router-dom'
+// import {auth} from '../../config/firebaseConfig'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {signOut} from 'firebase/auth'
 
 function Header({categories}) {
 
-  let navigate=useNavigate();
-
-    // const [user] = useAuthState(auth)
-    
-
-
-
-
+    let navigate=useNavigate();
+    // const [user]=useAuthState(auth)
   return (
     <div className='header-container'>
-
         <FaHome className='home-icon' onClick={()=> navigate('/')} />
 
+      <div className='categories-container'>
         {
-            user
-            ? <Link className='auth-link' to ='/addarticle'>Add Article</Link>
-            : null
+            categories.map((item, index)=>{
+                return <Link key={index} className='nav-link' to={`/category/${item}`}>{item}</Link>
+            })
         }
+      </div>
 
-        <div className='categories-container'>
-        {
-
-          categories.map((item, index)=>{
-              return <Link key={index} className='nav-link' to={`/category/${item}`}>{item}</Link>
-          })
-          }
-
-        </div>
-
-        {
-          user
-          ? <div>
-            <span className="username">{user?.displayName ? user?.displayName : user?.email}</span>
-            <button onClick={()=>signOut(auth)} className="auth-link">Logout</button>
-          </div>
-          : <Link className='auth-link' to={'/auth'}>Sign up</Link>
-        }
-      
+      {/* {
+      user?  */}
+      <div>
+        {/* <span className='username'>{user?.displayName ? user?.displayName : user?.email} </span> */}
+        {/* <button onClick={()=>signOut(auth)} className='auth-link'>Logout</button> */}
+      </div>
+       <Link className='auth-link' to={'/auth'}> Signup </Link>        
     </div>
   )
 }
