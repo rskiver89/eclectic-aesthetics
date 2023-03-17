9156992ec6eebbef4126acdc67343e06ef83df18
rskiver89 <108497515+rskiver89@users.noreply.github.com>
2023-03-17 12:40:59 -0400

    add articles

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,13 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import './Banner.css'
+import {db} from '../../config/firebaseConfig'
+import {getStorage, ref, list} from 'firebase/storage'
+import { useNavigate } from 'react-router-dom'
+import {getDocs, query, collection, orderBy, limit} from 'firebase/firestore'
 
 function Banner() {
     const [mainArticle, setMainArticle]=useState('')
     const [otherArticles, setOtherArticles]=useState('')
+    let navigate=useNavigate()
+
+    useEffect(()=>{
+      const articleRef = collection(db, 'articles')
+      const q = query(articleRef, orderBy('createdAt', 'desc'), limit(5))
+      getDocs(q, articleRef)
+      .then(res => {
+        const articles = res.docs.map(item => ({
+          id: item.id,
+          ...item.data()
+  
+      }))
+          setMainArticle(articles[0])
+          setOtherArticles(articles?.splice(1))
+    })
+    }, [])
 
 
   return (
-    <div>
-      
+    <div className='banner-info'>
+      <h2>{mainArticle?.title}...</h2>
+      <small>{mainArticle?.createdAt?.toDate().toDateString()}</small>
     </div>
   )
 }
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Banner from '../../components/Banner/Banner'
+import {db} from '../../config/firebaseConfig'
+import './Homepage.css'
+import {getDocs, query, collection, where, limit} from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
+
 
 function Homepage() {
+
+  const [selectedArticles, setSelectedArticles]=useState([])
+  let navigate=useNavigate()
+
+  useEffect(()=>{
+    const articleRef = collection(db, 'articles')
+    const q = query(articleRef, where('isSelected', '==', true), limit(5))
+    getDocs(q, articleRef)
+    .then(res => {
+      const articles = res.docs.map(item => ({
+        id: item.id,
+        ...item.data()
+
+    }))
+        setSelectedArticles(articles)
+  })
+  }, [])
+
+
+
   return (
     <div>
-      Homepage
+      <Banner />
+      <div className='selected-articles'>
+        <h1>Editors Choice</h1>
+        {
+          selectedArticles?.map(item => {
+            return <div key={item.id}>
+              <p>{item.title}</p>
+              <img src={item.imageUrl} style={{width: '200px'}} />
+              <p onClick={() => navigate(`/article/${item.id}`)}>Read</p>
+            </div>
+          })
+        }
+
+      </div>
+
     </div>
   )
 }
